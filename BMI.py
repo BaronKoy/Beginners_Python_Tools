00557e2cfe2ae2e1ca6a6b65d84b7c7dda494f65
Baron Koylass <baron.koylass.16@ucl.ac.uk>
2017-05-29 02:11:45 +0100

    Simple BMI Calculator

---
+++ BMI.py
@@ -0,0 +1,21 @@
+#Simple BMI Calculator
+#Written 29/05/2017, Baron Koylass
+
+print 'The simple BMI Calculator!'
+
+weight=float(input('What is your weight in kg? '))
+height=float(input('What is your height in Metres? '))
+BMI=weight/(height*height)
+
+if BMI<=18.5:
+    print 'Your body mass index is', BMI , 'this is classed as underweight'
+elif BMI>18.5 and BMI<=25:
+    print 'Your body mass index is', BMI ,'this is classed as weight'
+elif BMI>25 and BMI<=30:
+    print 'Your body mass index is', BMI ,'this is classed as overweight'
+elif BMI>30:
+    print 'Your body mass index is', BMI ,'this is classed as obese'
+else:
+    print 'There was an error with your input'
+
+print 'Thank you for using the simple BMI Calculator'
